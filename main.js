61da3e0545379fa4b1da352d5c45081633c46cc3
ElenaSchubel <ebassist@live.com>
2017-04-19 15:49:27 +1200

    working bouncing ball

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext("3d"); 
+var ctx = canvas.getContext('2d');
 var raf;
 var running = false;
 
@@ -18,6 +18,50 @@ var ball = {
     ctx.fill();
   }
 };
+
+function clear() {
+  ctx.fillStyle = 'rgba(255, 255, 255, 0.3)';
+  ctx.fillRect(0,0,canvas.width,canvas.height);
+}
+
+function draw() {
+  clear();
+  ball.draw();
+  ball.x += ball.vx;
+  ball.y += ball.vy;
+
+  if (ball.y + ball.vy > canvas.height || ball.y + ball.vy < 0) {
+    ball.vy = -ball.vy;
+  }
+  if (ball.x + ball.vx > canvas.width || ball.x + ball.vx < 0) {
+    ball.vx = -ball.vx;
+  }
+
+  raf = window.requestAnimationFrame(draw);
+}
+
+canvas.addEventListener('mousemove', function(e) {
+  if (!running) {
+    clear();
+    ball.x = e.clientX;
+    ball.y = e.clientY;
+    ball.draw();
+  }
+});
+
+canvas.addEventListener('click', function(e) {
+  if (!running) {
+    raf = window.requestAnimationFrame(draw);
+    running = true;
+  }
+});
+
+canvas.addEventListener('mouseout', function(e) {
+  window.cancelAnimationFrame(raf);
+  running = false;
+});
+
+ball.draw();
 
 function clear() {
   ctx.fillStyle = 'rgba(255, 255, 255, 0.3)';
